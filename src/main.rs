3acb6587e80842a9112d4a5d06f486fd4744e59f
Brian Merchant <bhmerchant@gmail.com>
2023-08-01 10:14:09 -0700

    Seems like splitting up into multiple FSTs is a dead end in terms of mapping a value?

--- src/main.rs
+++ src/main.rs
@@ -1,149 +1,179 @@
-use chrono::{DateTime, Utc};
 use fst::Map;
-use rkyv::AlignedVec;
-use time::TimeString;
-use std::fs::{read_dir, DirEntry};
+// use rkyv::{Archive, Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs::FileType as StdFileType;
+use std::fs::{read_dir, DirEntry, Metadata};
 use std::io::Result as IoResult;
-use std::path::{Path, PathBuf};
-use std::time::SystemTime;
-
-use rkyv::{
-    collections::hash_map::ArchivedHashMap, vec::ArchivedVec, Archive, Deserialize, Serialize,
-};
+use std::path::{Component, Path, PathBuf};
+use time::TimeString;
 
 mod time;
 
-
-
-type FstBytes = AlignedVec;
-
-#[derive(Archive, Serialize, Deserialize)]
-struct PathIndexer {
-    automata: Vec<FstMap>,
+struct PathIndex {
+    index_maps: Vec<Map<Vec<u8>>>,
+    meta_store: MetaStore,
 }
 
-impl PathIndexer {
-    fn get_index(&self, path: &Path) -> usize {
+impl PathIndex {
+    fn get_index(&self, path: &Path) -> Option<usize> {
+        unimplemented!()
+    }
+}
+
+pub struct SplitPath {
+    path: PathBuf,
+    components: Vec<String>,
+    fst_outputs: Option<Vec<usize>>,
+}
+
+impl From<&Path> for SplitPath {
+    fn from(path: &Path) -> Self {
+        Self {
+            path: path.to_path_buf(),
+            components: path
+                .components()
+                .map(|c| c.as_os_str().to_string_lossy().to_string())
+                .collect(),
+            fst_outputs: None,
+        }
+    }
+
+    fn gen_fst_outputs(&mut self, index: usize) {}
+}
+
+pub struct PathIndexBuilder {
+    paths: Vec<SplitPath>,
+    meta_store: MetaStore,
+}
+
+impl PathIndexBuilder {
+    fn insert_path(&mut self, path: &Path) {
+        self.paths.push(path.into());
+    }
+}
+
+impl From<PathIndexBuilder> for PathIndex {
+    fn from(mut builder: PathIndexBuilder) -> Self {
+        builder
+            .paths
+            .iter_mut()
+            .for_each(|path| path.gen_fst_outputs(builder.meta_store.insert(path.path.as_path())));
+        for (path, components) in builder
+            .paths
+            .into_iter()
+            .zip(builder.path_components.into_iter())
+        {
+            components.sort_unstable();
+            builder.meta_store.insert(&path);
+        }
         unimplemented!()
     }
 }
 
 // #[derive(Archive, Serialize, Deserialize)]
-struct PathStore {
-    store: ArchivedHashMap<usize, Option<PathMetadata>>,
+struct MetaStore {
+    count: usize,
+    store: HashMap<usize, Option<PathMeta>>,
+    free_indices: Vec<usize>,
 }
 
+impl MetaStore {
+    fn generate_index(&mut self) -> usize {
+        self.free_indices.pop().unwrap_or_else(|| {
+            let index = self.count;
+            self.count += 1;
+            index
+        })
+    }
+
+    fn insert(&mut self, path: &Path) -> usize {
+        let index = self.generate_index();
+        self.store
+            .insert(index, path.metadata().ok().map(PathMeta::from))
+            .unwrap();
+        index
+    }
+}
+
+// #[derive(Archive, Serialize, Deserialize)]
 enum FileType {
     Directory,
-    File { len: u64 },
-    SymLink { target: String },
+    File,
+    SymLink,
+    Unknown,
 }
 
-struct PathMetadata {
-    path: String,
+impl From<StdFileType> for FileType {
+    fn from(ft: StdFileType) -> Self {
+        if ft.is_dir() {
+            Self::Directory
+        } else if ft.is_file() {
+            Self::File
+        } else if ft.is_symlink() {
+            Self::SymLink
+        } else {
+            Self::Unknown
+        }
+    }
+}
+
+// #[derive(Archive, Serialize, Deserialize)]
+struct PathMeta {
     file_type: FileType,
-    modified:
+    modified: Option<String>,
+    accessed: Option<String>,
+    created: Option<String>,
 }
 
-
-
-
-// source
-// pub fn permissions(&self) -> Permissions
-
-// Returns the permissions of the file this metadata is for.
-// Examples
-
-// use std::fs;
-
-// fn main() -> std::io::Result<()> {
-//     let metadata = fs::metadata("foo.txt")?;
-
-//     assert!(!metadata.permissions().readonly());
-//     Ok(())
-// }
-
-// 1.10.0 · source
-// pub fn modified(&self) -> Result<SystemTime>
-
-// Returns the last modification time listed in this metadata.
-
-// The returned value corresponds to the mtime field of stat on Unix platforms and the ftLastWriteTime field on Windows platforms.
-// Errors
-
-// This field might not be available on all platforms, and will return an Err on platforms where it is not available.
-// Examples
-
-// use std::fs;
-
-// fn main() -> std::io::Result<()> {
-//     let metadata = fs::metadata("foo.txt")?;
-
-//     if let Ok(time) = metadata.modified() {
-//         println!("{time:?}");
-//     } else {
-//         println!("Not supported on this platform");
-//     }
-//     Ok(())
-// }
-
-// 1.10.0 · source
-// pub fn accessed(&self) -> Result<SystemTime>
-
-// Returns the last access time of this metadata.
-
-// The returned value corresponds to the atime field of stat on Unix platforms and the ftLastAccessTime field on Windows platforms.
-
-// Note that not all platforms will keep this field update in a file’s metadata, for example Windows has an option to disable updating this time when files are accessed and Linux similarly has noatime.
-// Errors
-
-// This field might not be available on all platforms, and will return an Err on platforms where it is not available.
-// Examples
-
-// use std::fs;
-
-// fn main() -> std::io::Result<()> {
-//     let metadata = fs::metadata("foo.txt")?;
-
-//     if let Ok(time) = metadata.accessed() {
-//         println!("{time:?}");
-//     } else {
-//         println!("Not supported on this platform");
-//     }
-//     Ok(())
-// }
-
-// 1.10.0 · source
-// pub fn created(&self) -> Result<SystemTime>
-
-// Returns the creation time listed in this metadata.
-
-// The returned value corresponds to the btime field of statx on Linux kernel starting from to 4.11, the birthtime field of stat on other Unix platforms, and the ftCreationTime field on Windows platforms.
-// Errors
-
-// This field might not be available on all platforms, and will return an Err on platforms or filesystems where it is not available.
-// Examples
-
-// use std::fs;
-
-// fn main() -> std::io::Result<()> {
-//     let metadata = fs::metadata("foo.txt")?;
-
-//     if let Ok(time) = metadata.created() {
-//         println!("{time:?}");
-//     } else {
-//         println!("Not supported on this platform or filesystem");
-//     }
-//     Ok(())
-// }
-
-// struct NestedFst {
-//     fst: IndexFst,
-//     outputs: Vec<FstOut>,
-// }
+impl From<Metadata> for PathMeta {
+    fn from(metadata: Metadata) -> Self {
+        Self {
+            file_type: metadata.file_type().into(),
+            modified: metadata
+                .modified()
+                .ok()
+                .map(|sys_time| TimeString::from(sys_time).into()),
+            accessed: metadata
+                .accessed()
+                .ok()
+                .map(|sys_time| TimeString::from(sys_time).into()),
+            created: metadata
+                .created()
+                .ok()
+                .map(|sys_time| TimeString::from(sys_time).into()),
+        }
+    }
+}
 
 fn queue_fst_creation(path: PathBuf) {
-    unimplemented!();
+    unimplemented!()
+}
+
+fn normalize(path: &Path) -> PathBuf {
+    let mut components = path.components().peekable();
+    let mut ret = if let Some(c @ Component::Prefix(..)) = components.peek().cloned() {
+        components.next();
+        PathBuf::from(c.as_os_str())
+    } else {
+        PathBuf::new()
+    };
+
+    for component in components {
+        match component {
+            Component::Prefix(..) => unreachable!(),
+            Component::RootDir => {
+                ret.push(component.as_os_str());
+            }
+            Component::CurDir => {}
+            Component::ParentDir => {
+                ret.pop();
+            }
+            Component::Normal(c) => {
+                ret.push(c);
+            }
+        }
+    }
+    ret
 }
 
 fn main() {
@@ -152,18 +182,21 @@ fn main() {
         .expect("Could not open C:\\")
         .collect::<IoResult<Vec<DirEntry>>>()
         .unwrap();
+
+    let per_segment_fst: Vec<Map<Vec<u8>>> = vec![];
+
     for entry in drive_entries.iter() {
-        let path = entry.path();
+        let path = normalize(&entry.path());
         let metadata = entry
             .metadata()
             .map_err(|io_err| {
                 format!(
-                    "Could not get metadata for path {}. Reason: {}",
-                    path.to_string_lossy(),
-                    io_err
+                    "Could not get metadata for path {}. Reason: {io_err}",
+                    path.to_string_lossy()
                 )
             })
             .unwrap();
+        for component in path.components() {}
         if metadata.is_dir() {
             queue_fst_creation(path)
         }
--- src/time.rs
+++ src/time.rs
@@ -1,16 +1,25 @@
+use chrono::{DateTime, Utc};
+use rkyv::{Archive, Deserialize, Serialize};
 use std::time::SystemTime;
 
+#[derive(Archive, Serialize, Deserialize)]
 pub struct TimeString(pub String);
 
 impl From<&TimeString> for String {
-    fn from(time: &TimeString) -> Self {
-        time.0.clone()
+    fn from(time_str: &TimeString) -> Self {
+        time_str.0.clone()
     }
 }
 
 impl From<SystemTime> for TimeString {
-    fn from(time: SystemTime) -> Self {
-        let dt: DateTime<Utc> = time.clone().into();
+    fn from(sys_time: SystemTime) -> Self {
+        let dt: DateTime<Utc> = sys_time.clone().into();
         TimeString(format!("{}", dt.format("%Y-%b-%d %H:%M")))
+    }
+}
+
+impl From<TimeString> for String {
+    fn from(time_str: TimeString) -> Self {
+        time_str.0
     }
 }
